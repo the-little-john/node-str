4218a7f6bb5f702e6b9add07ab8ac171cb8262e5
JohnJr15 <joao.jr.etec@gmail.com>
2020-02-09 00:09:10 -0300

    Gerenciando erros do servidor

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
-
 const app = express(); // Instância do express
 const port = normalizePort(process.env.PORT || '3000');
+
 app.set('port', port); // Seta a porta
 
 const server = http.createServer(app); // Cria servidor baseado no express (Modelo MVC)
@@ -21,7 +21,9 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
-console.log('API rodando na porta ' + port);
+server.on('error', onError);
+
+//console.log('API rodando na porta ' + port);
 
 function normalizePort(val){ // Retirada do gerador de código do express
     const port = parseInt(val, 10);
@@ -35,4 +37,27 @@ function normalizePort(val){ // Retirada do gerador de código do express
     }
 
     return false;
+}
+
+function onError(error) { // Retirada do gerador de código do express. Tratamento de erros  
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ? // Monta mensagem de erro
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code){
+        case 'EACCES': // Erro de permissão
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE': // Endereço em uso
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
 }
