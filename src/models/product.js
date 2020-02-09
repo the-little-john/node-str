94036b74b1127643884e4ea43dfc5d74164d13c8
JohnJr15 <joao.jr.etec@gmail.com>
2020-02-09 18:17:06 -0300

    Models

---
+++ src/models/product.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    // _id (Gerado automaticamente)
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: { // Cadeira Gamer -> cadeira-gamer(slug)
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true 
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    tags: [{ // Array de strings que permite incluirmos diversos valores como tags
+        type: String,
+        required: true
+    }]
+});
+
+module.exports = mongoose.model('Product', schema);  // Exportando model Product
