3fba9448d7147a1f94385c938225b39632a9d7be
JohnJr15 <joao.jr.etec@gmail.com>
2020-02-09 00:21:15 -0300

    Iniciando o Debug

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
-//console.log('API rodando na porta ' + port);
+console.log('API rodando na porta ' + port);
 
 function normalizePort(val){ // Retirada do gerador de código do express
     const port = parseInt(val, 10);
@@ -60,4 +61,12 @@ function onError(error) { // Retirada do gerador de código do express. Tratamen
         default:
             throw error;
     }
+}
+
+function onListening(){
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr 
+        : 'port ' + addr.port;
+    debug('Listening on'+ bind);
 }
