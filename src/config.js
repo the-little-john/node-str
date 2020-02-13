3ee404c4fe55ab90a5e5567d51cb9bb44b4dedce
JohnJr15 <joao.jr.etec@gmail.com>
2020-02-13 00:47:55 -0300

    Enviando e-mail de boas vindas

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef'; // Chave
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
     connectionString: 'mongodb+srv://joaozinho:joaozinho@cluster0-ah95t.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridKey: 'TBD',
+    sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -3,9 +3,11 @@
 const ValidationContract = require('../validators/fluent-validator');
 const repository = require('../repositories/customer-repository');
 const md5 = require('md5');
+const emailService = require('../services/email-service');
+
 
 exports.post = async (req, res, next) => {
-    let contract = new ValidationContract(); 
+    let contract = new ValidationContract();
     contract.hasMinLen(req.body.name, 3, 'O nome deve conter pelo menos 3 caracteres');
     contract.isEmail(req.body.email, 'E-mail inválido');
     contract.hasMinLen(req.body.password, 6, 'A senha deve conter pelo menos 6 caracteres');
@@ -20,8 +22,13 @@ exports.post = async (req, res, next) => {
         await repository.create({
             name: req.body.name,
             email: req.body.email,
-            password: md5(req.body.password + global.SALT_KEY) 
+            password: md5(req.body.password + global.SALT_KEY)
         });
+
+        emailService.send(req.body.email,
+            'Bem vindo ao Node Store',
+            global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({
             message: 'Cliente cadastrado com sucesso!'
         });
